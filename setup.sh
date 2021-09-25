ae3dfd452ca3348758c99190d3ccc0427684e1a4
Baschtie <Baschtie@users.noreply.github.com>
2021-09-25 16:26:00 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshenv ~/.zshenv
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.zsh ~/.zsh
