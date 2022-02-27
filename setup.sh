3fa3c1345716d5aaec384c5e8e01955bf10e0024
Baschtie <Baschtie@users.noreply.github.com>
2022-02-27 09:39:55 +0100

    add vsc

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.zsh ~/.zsh
+ln -s ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
